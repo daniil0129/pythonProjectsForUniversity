01851a31a766c5a5e1c50c4f763a46a31ee9c9bf
Daniil <daniil.s.h.e.p.k.o.v@gmail.com>
2024-11-11 13:24:39 +0300

    Переменные

---
+++ Modul' 1 Praktika GIT Bazovyye struktury dannykh/module_1_6.py
@@ -0,0 +1,10 @@
+quantity_of_completions_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / quantity_of_completions_tasks
+
+print("Курс:", course_name,", всего задач:",quantity_of_completions_tasks,", затрачено часов:",hours_spent,
+      ", среднее время выполнения ",time_per_task ," часа.")
+
+
+
